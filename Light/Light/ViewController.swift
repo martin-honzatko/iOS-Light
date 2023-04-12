65c87e64652b3fe15f2ce2c89827e179184067ff
Martin Honzátko <43739467+martin-honzatko@users.noreply.github.com>
2023-04-12 10:59:09 -0700

    Add working light

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	var lightOn = false
+	//@IBOutlet var lightButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		updateUI()
 	}
 
-
+	fileprivate func updateUI() {
+		view.backgroundColor = lightOn ? .white : .black
+//		if lightOn {
+//			view.backgroundColor = .white
+//			lightButton.setTitle("Off", for: .normal)
+//		} else {
+//			view.backgroundColor = .black
+//			lightButton.setTitle("On", for: .normal)
+//		}
+	}
+	
+	@IBAction func buttonPressed(_ sender: Any) {
+		lightOn.toggle()
+		
+		updateUI()
+	}
+	
 }
 
